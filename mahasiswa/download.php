59edca3dcba8c577ecda9fc116bda4da75e8feb6
arya114 <alrqkiananda@student.unmul.ac.id>
2024-09-13 08:33:21 +0800

    Refactor vendor dependencies and add ilovepdf/ilovepdf-php library

--- mahasiswa/download.php
+++ mahasiswa/download.php
@@ -6,34 +6,45 @@ use PhpOffice\PhpWord\TemplateProcessor;
 // Memulai sesi untuk mengambil data pengguna
 session_start();
 
+function hariIndonesia($day) {
+    $hari = [
+        'Sunday' => 'Minggu',
+        'Monday' => 'Senin',
+        'Tuesday' => 'Selasa',
+        'Wednesday' => 'Rabu',
+        'Thursday' => 'Kamis',
+        'Friday' => 'Jumat',
+        'Saturday' => 'Sabtu'
+    ];
+    return $hari[$day];
+}
+
 // Cek apakah user telah login
 if (!isset($_SESSION['nim']) || $_SESSION['role'] !== 'mahasiswa') {
     header("Location: login.php");
     exit;
 }
 
-// Ambil data pengguna dari database
+// Koneksi ke database
+require '../config/database.php';
+
+// Ambil data pengguna berdasarkan NIM
 $nim = $_SESSION['nim'];
+$sql = "SELECT * FROM users WHERE nim = '$nim'";
+$result = $conn->query($sql);
 
-// Koneksi ke database (ganti sesuai dengan konfigurasi database Anda)
-$mysqli = new mysqli('localhost', 'root', '', 'inven');
-
-if ($mysqli->connect_error) {
-    die("Connection failed: " . $mysqli->connect_error);
-}
-
-// Query untuk mengambil data pengguna
-$userQuery = $mysqli->prepare("SELECT name, telp, alamat FROM users WHERE nim = ?");
-$userQuery->bind_param('s', $nim);
-$userQuery->execute();
-$userQuery->store_result();
-
-// Pastikan data ditemukan
-if ($userQuery->num_rows > 0) {
-    $userQuery->bind_result($nama, $telepon, $alamat);
-    $userQuery->fetch();
+// Memeriksa apakah data ditemukan
+if ($result->num_rows > 0) {
+    // Mengambil data pengguna
+    $row = $result->fetch_assoc();
+    $name = $row['name'];
+    $alamat = $row['alamat'];
+    $telp = $row['telp'];
+    $prodi = $row['prodi'];
+    // Tambahkan data lainnya jika diperlukan
 } else {
-    die("Data pengguna tidak ditemukan.");
+    echo "Data pengguna tidak ditemukan.";
+    exit;
 }
 
 // Ambil data dari form (atau Anda bisa mengambilnya dari database jika diperlukan)
@@ -48,26 +59,37 @@ $jenis_ruangan = $_POST['jenis_ruangan'];
 $fasilitas = implode(", ", $_POST['fasilitas']);
 $tanggal_pengambilan = $_POST['tanggal_pengambilan'];
 $tanggal_pengembalian = $_POST['tanggal_pengembalian'];
+$today = hariIndonesia(date('l')) . ', ' . date('d-m-Y');
+$dateMulai = new DateTime($hari_tanggal_mulai);
+$dateSelesai = new DateTime($hari_tanggal_selesai);
+$interval = $dateMulai->diff($dateSelesai);
+$periode_peminjaman = $interval->days;
+
+
 
 // Buat instance TemplateProcessor dan buka template dokumen Word
 $templateProcessor = new TemplateProcessor('template.docx'); // Ganti dengan path template Anda
 
 // Mengganti placeholder dalam template dengan data input pengguna
 $templateProcessor->setValue('nim', $nim);
-$templateProcessor->setValue('name', $nama); // Data nama dari database
-$templateProcessor->setValue('telp', $telepon); // Data telepon dari database
-$templateProcessor->setValue('alamat', $alamat); // Data alamat dari database
+$templateProcessor->setValue('nama', $name); // Contoh mengganti dengan nama pengguna
+$templateProcessor->setValue('alamat', $alamat); // Contoh mengganti dengan alamat pengguna
+$templateProcessor->setValue('telp', $telp); // Contoh mengganti dengan telp pengguna
+$templateProcessor->setValue('prodi', $prodi); // Contoh mengganti dengan prodi pengguna
 $templateProcessor->setValue('nama_kegiatan', $nama_kegiatan);
-$templateProcessor->setValue('hari_tanggal_mulai', $hari_tanggal_mulai);
-$templateProcessor->setValue('hari_tanggal_selesai', $hari_tanggal_selesai);
+$templateProcessor->setValue('hari_tanggal_mulai', hariIndonesia(date('l', strtotime($hari_tanggal_mulai))) . ', ' . date('d-m-Y', strtotime($hari_tanggal_mulai)));
+$templateProcessor->setValue('hari_tanggal_selesai', hariIndonesia(date('l', strtotime($hari_tanggal_selesai))) . ', ' . date('d-m-Y', strtotime($hari_tanggal_selesai)));
+$templateProcessor->setValue('periode_peminjaman', $periode_peminjaman);
 $templateProcessor->setValue('waktu_mulai', $waktu_mulai);
 $templateProcessor->setValue('waktu_selesai', $waktu_selesai);
 $templateProcessor->setValue('jumlah_peserta', $jumlah_peserta);
 $templateProcessor->setValue('jenis_ruangan', $jenis_ruangan);
 $templateProcessor->setValue('fasilitas', $fasilitas);
-$templateProcessor->setValue('tanggal_pengambilan', $tanggal_pengambilan);
-$templateProcessor->setValue('tanggal_pengembalian', $tanggal_pengembalian);
+$templateProcessor->setValue('tanggal_pengambilan', hariIndonesia(date('l', strtotime($tanggal_pengambilan))) . ', ' . date('d-m-Y', strtotime($tanggal_pengambilan)));
+$templateProcessor->setValue('tanggal_pengembalian', hariIndonesia(date('l', strtotime($tanggal_pengembalian))) . ', ' . date('d-m-Y', strtotime($tanggal_pengembalian)));
 $templateProcessor->setValue('jenis_peminjaman', $jenis_peminjaman);
+$templateProcessor->setValue('tanggal_hari_ini', $today);
+
 
 // Nama file untuk diunduh
 $fileName = 'Peminjaman_Lab_' . $nim . '.docx';
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,6 +33,25 @@ class ComposerAutoloaderInit4c9a42706ae585541952a094714e3b3a
 
         $loader->register(true);
 
+        $includeFiles = \Composer\Autoload\ComposerStaticInit4c9a42706ae585541952a094714e3b3a::$files;
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire4c9a42706ae585541952a094714e3b3a($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+/**
+ * @param string $fileIdentifier
+ * @param string $file
+ * @return void
+ */
+function composerRequire4c9a42706ae585541952a094714e3b3a($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+        require $file;
     }
 }
